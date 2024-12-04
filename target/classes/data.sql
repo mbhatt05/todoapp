119d6261c7669b64e84e3d31049243dea0369693
mbhatt05 <65867269+mbhatt05@users.noreply.github.com>
2024-12-04 13:38:42 -0500

    Add files via upload

---
+++ target/classes/data.sql
@@ -0,0 +1,13 @@
+CREATE TABLE TODO (
+
+    TITLE VARCHAR(255),
+    DESCRIPTION VARCHAR(255),
+    COMPLETED BOOLEAN
+);
+
+
+
+
+INSERT INTO TODO (TITLE, DESCRIPTION, COMPLETED) VALUES ('Buy groceries', 'Milk, Bread, Eggs, and Vegetables', false);
+INSERT INTO TODO ( TITLE, DESCRIPTION, COMPLETED) VALUES ('Pay bills', 'Electricity and Water bills', false);
+INSERT INTO TODO ( TITLE, DESCRIPTION, COMPLETED) VALUES ('Go to gym', 'Evening workout', true);
